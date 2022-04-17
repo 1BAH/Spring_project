aa9ae926acaa0e216bab414f82616a0faa07ea9e
Iван <kalinin.is@phystech.edu>
2022-04-17 23:05:17 +0300

    client form and view page updated

--- src/main/java/com/example/banksystem/controllers/ClientController.java
+++ src/main/java/com/example/banksystem/controllers/ClientController.java
@@ -3,6 +3,7 @@ package com.example.banksystem.controllers;
 import com.example.banksystem.models.Client;
 import com.example.banksystem.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.restart.RestartEndpoint;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,14 @@ public class ClientController {
     @Autowired
     ClientRepository clientRepository;
 
+    @Autowired
+    private RestartEndpoint restartEndpoint;
+
     @GetMapping("/registration")
     public String Registration(Model model) {
         return "registration";
     }
+
     @GetMapping("/registration/form")
     public String addClient(@RequestParam String name, @RequestParam String surname, @RequestParam String passport, @RequestParam String address) {
         Client client = new Client();
@@ -26,6 +31,8 @@ public class ClientController {
         client.setAddress(address);
 
         clientRepository.save(client);
+
+        restartEndpoint.restart();
 
         return "redirect:/";
     }
