f6cfe98d1cc3758acd2dc5e703f021024e916d99
Iван <kalinin.is@phystech.edu>
2022-05-09 19:50:32 +0300

    Bank controller tests added

---
+++ src/test/java/com/example/banksystem/BankTest.java
@@ -0,0 +1,126 @@
+package com.example.banksystem;
+
+import com.example.banksystem.models.Bank;
+import com.example.banksystem.models.Client;
+import com.example.banksystem.repositories.AccountRepository;
+import com.example.banksystem.repositories.BankRepository;
+import com.example.banksystem.repositories.ClientRepository;
+import com.example.banksystem.repositories.TransactionRepository;
+import com.example.banksystem.securityconfig.CustomAuthenticationEntryPoint;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class BankTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    ClientRepository clientRepository;
+
+    @MockBean
+    AccountRepository accountRepository;
+
+    @MockBean
+    BankRepository bankRepository;
+
+    @MockBean
+    TransactionRepository transactionRepository;
+
+    @MockBean
+    CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Test
+    @WithMockUser(username = "user", password = "pass")
+    public void banks() throws Exception {
+        Client client = new Client(3,"user", "sur", "add", "pass");
+
+        ArrayList<Bank> banks = new ArrayList<>();
+
+        Bank bank1 = new Bank("TestBank1", 1);
+        banks.add(bank1);
+        Bank bank2 = new Bank("TestBank2", 2);
+        banks.add(bank2);
+
+        Mockito.when(clientRepository.findByName(Mockito.any())).thenReturn(client);
+        Mockito.when(bankRepository.findAll()).thenReturn(banks);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/banks");
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("banks", banks))
+                .andExpect(model().attribute("user", client));
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "pass")
+    public void redirect() throws Exception {
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .get("/banks/add/form")
+                .param("name", "TestBank")
+                .param("percentage", "3");
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/banks"));
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "pass")
+    public void nameIsEmpty() throws Exception {
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .get("/banks/add/form")
+                .param("percentage", "3");
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().is4xxClientError());
+    }
+
+
+    @Test
+    @WithMockUser(username = "user", password = "pass")
+    public void percentageIsEmpty() throws Exception {
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .get("/banks/add/form")
+                .param("name", "TestBank");
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockUser(username = "user", password = "pass")
+    public void bankAdd() throws Exception {
+        Client client = new Client(3,"user", "sur", "add", "pass");
+
+        Mockito.when(clientRepository.findByName(Mockito.any())).thenReturn(client);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.get("/banks/add");
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("title", "Bank creation"))
+                .andExpect(model().attribute("user", client));
+    }
+}
